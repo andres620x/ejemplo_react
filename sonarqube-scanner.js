f752aa9056c106e6eaa974cec9a88591265226c8
Andres <jorge-620@hotmail.com>
2022-08-13 08:44:54 -0500

    Subida de fuentes

---
+++ sonarqube-scanner.js
@@ -0,0 +1,18 @@
+const scanner = require('sonarqube-scanner');
+scanner(
+    {
+        serverUrl: "http://localhost:9000",
+        login: "admin",
+        password: "123456789",
+        options: {
+            "sonar.sources": "./src",
+            "sonar.exclusions": "**/*.test.tsx",
+            "sonar.tests": "./src",
+            "sonar.test.inclusions": "**/*.test.tsx,**/*.test.ts",
+            "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+            "sonar.testExecutionReportPaths": "test-report.xml",
+            "sonar.eslint.reportPaths":"eslint-report.json"
+        },
+    },
+    () => process.exit()
+);
